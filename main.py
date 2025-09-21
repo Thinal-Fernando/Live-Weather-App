008950a938343adb86bf9220631fe6b94ff52eb7
Thinal <r.thinal.fernando@gmail.com>
2025-09-21 18:55:48 +0530

    - created a function to get weather information - Added Error check to see if request was good - convert the request data to json to save to a pd df later

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("OpenWeather_API_KEY")
-city= "Colombo"
-r = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric")
 
-print(r.text)
+
+def get_weather(city):
+    r = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric")
+
+    if r.status_code != 200:
+        print('Error Occured') 
+    
+    data = r.json()  # Cleaning the data to add to a pandas df
+
+    print(data)
+
+
+
+get_weather("Colombo")
